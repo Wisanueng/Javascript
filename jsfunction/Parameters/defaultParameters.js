e272cab296c9949f4b1655caae8f257cdc0d0b10
Wisanueng <wisanunewstudy2567@gmail.com>
2025-03-23 23:50:01 +0700

    เหลือข้อ 4,5,6,7,8 ในไฟล์ tumto.txt

---
+++ jsfunction/Parameters/defaultParameters.js
@@ -0,0 +1,7 @@
+
+//กำหนดค่าเข้าไปใน parameters ตรงๆ
+function Hi (name = "kwan"){
+    console.log(`hello ${name}`);
+}
+
+console.log(Hi);
---
+++ jsfunction/Parameters/index.js
@@ -0,0 +1,9 @@
+//parameters ในที่นี้คือ name
+//parameters คือค่าที่ถูกส่งเข้าไปใน function
+
+
+function greet (name){
+    console.log(`hello ${name}`);
+}
+
+greet("alice");
